ff2470a591de8b647df0df97caaf766383cb9980
ameda-kevin <amedakevin@gmail.com>
2024-06-19 09:16:38 +0300

    custom authentication implementation is done

--- src/main/java/com/ameda/kevin/security_reactjs/domain/ApiAuthentication.java
+++ src/main/java/com/ameda/kevin/security_reactjs/domain/ApiAuthentication.java
@@ -7,8 +7,10 @@ package com.ameda.kevin.security_reactjs.domain;/*
 */
 
 import com.ameda.kevin.security_reactjs.domain.dto.User;
+import com.ameda.kevin.security_reactjs.exceptions.APIException;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 
 import java.util.Collection;
 
@@ -20,7 +22,20 @@ public class ApiAuthentication extends AbstractAuthenticationToken {
     private String password;
     private boolean authenticated;
 
-    public ApiAuthentication(User user,  Collection<? extends GrantedAuthority> authorities) {
+    /*
+    * constructor called for unauthenticated user
+    * */
+    private ApiAuthentication(String email, String password) {
+        super(AuthorityUtils.NO_AUTHORITIES);
+        this.email = email;
+        this.password = password;
+        this.authenticated = false;
+    }
+    /*
+    * constructor called for authenticated user
+    * constructors are private for users to use the helper methods
+    * */
+    private ApiAuthentication(User user,  Collection<? extends GrantedAuthority> authorities) {
         super(authorities);
         this.user = user;
         this.password = PASSWORD_PROTECTED;
@@ -28,13 +43,50 @@ public class ApiAuthentication extends AbstractAuthenticationToken {
         this.authenticated = true;
     }
 
+    public static ApiAuthentication unauthenticated (String email,String password){
+        return new ApiAuthentication(email,password);
+    }
+
+    public static ApiAuthentication authenticated(User user,  Collection<? extends GrantedAuthority> authorities){
+        return new ApiAuthentication(user,authorities);
+    }
+
     @Override
     public Object getCredentials() {
-        return null;
+        return PASSWORD_PROTECTED;
+    }
+
+    /*
+   *
+   @author ameda
+   @project security-reactjs
+   *
+   */
+    @Override
+    public void setAuthenticated(boolean authenticated) {
+        throw new APIException("Oops!!!, one cannot set authentication.");
+    }
+
+    /*
+   *
+   @author ameda
+   @project security-reactjs
+   *
+   */
+    @Override
+    public boolean isAuthenticated() {
+        return this.authenticated;
     }
 
     @Override
     public Object getPrincipal() {
-        return null;
+        return this.user;
     }
+    public String getPassword(){
+        return this.password;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+
 }
